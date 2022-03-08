92e8176add8510bd206d28c98cbfc9fb3dded1f5
Yuliya Shatokhina <59236891+YuShato@users.noreply.github.com>
2022-03-09 00:10:24 +0300

    feat: add positions reducer

--- src/components/JobList.jsx
+++ src/components/JobList.jsx
@@ -1,14 +1,16 @@
-import data from '../mock/data.json';
-import { JobPosition } from './JobPosition';
+import { JobPosition } from './JobPosition'
+import { useSelector } from 'react-redux'
+import { selectAllpositions } from 'store/positions/position-selectors'
 
 const JobList = () => {
+  const positions = useSelector(selectAllpositions)
   return (
     <div className='job-list'>
-      {data.map(item => (
+      {positions.map(item => (
         <JobPosition key={item.id} {...item} />
       ))}
     </div>
   )
 }
 
-export {JobList};
+export { JobList }
